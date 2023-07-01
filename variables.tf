ba54c63b078c9b235c96a552e0bb946c427d151e
voloshyn.artem@gmail.com <voloshyn.artem@gmail.com>
2023-07-01 18:26:32 +0000

    add files

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "GOOGLE_REGION" {
+  type        = string
+  description = "Google region name to use"
+}
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "Google project id value to use"
+}
+variable "GKE_NUM_NODES" {
+  type        = number
+  description = "Number of nodes to use"
+}
---
+++ vars.tfvars
@@ -0,0 +1,3 @@
+GOOGLE_REGION  = "us-central1-c"
+GOOGLE_PROJECT = "round-bloom-221409"
+GKE_NUM_NODES  = 2
