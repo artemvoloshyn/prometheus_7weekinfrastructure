b735768afae1623f7ff553aba28153cd7a637832
voloshyn.artem@gmail.com <voloshyn.artem@gmail.com>
2023-07-02 21:30:59 +0000

    initial

--- variables.tf
+++ variables.tf
@@ -10,3 +10,35 @@ variable "GKE_NUM_NODES" {
   type        = number
   description = "Number of nodes to use"
 }
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type        = string
+  default     = "clusters"
+  description = "Flux manifests subdirectory"
+}
+
+# variable "github_owner" {
+#   type        = string
+#   description = "The GitHub owner"
+# }
+
+# variable "github_token" {
+#   type        = string
+#   description = "GitHub personal access token"
+# }
--- vars.tfvars
+++ vars.tfvars
@@ -1,3 +1,6 @@
-GOOGLE_REGION  = "us-central1-c"
-GOOGLE_PROJECT = "round-bloom-221409"
-GKE_NUM_NODES  = 2
+GOOGLE_REGION    = "us-central1-c"
+GOOGLE_PROJECT   = "round-bloom-221409"
+GKE_NUM_NODES    = 2
+FLUX_GITHUB_REPO = "flux-sops-gitops"
+GITHUB_OWNER     = "artemvoloshyn"
+GITHUB_TOKEN     = "../GITHUB_TOKEN"
